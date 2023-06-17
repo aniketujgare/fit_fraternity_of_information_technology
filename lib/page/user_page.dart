67bb24d996346f5301c86df94b5cd21f9ff84e8c
tugshinde <tugshinde@gmail.com>
2023-06-17 10:10:41 +0530

    user page

--- lib/page/user_page.dart
+++ lib/page/user_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fit_fraternity_of_information_technology/screens/sign_in/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import '../screens/sign_in/components/sign_form.dart';
+import '../size_config.dart';
 
 class UserPage extends StatelessWidget {
   const UserPage({super.key});
@@ -9,12 +9,25 @@ class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FirebaseAuth auth = FirebaseAuth.instance;
+    SizeConfig().init(context);
     return Scaffold(
-      backgroundColor: const Color(0xffF7F7F7),
+      // backgroundColor: const Color(0xffF7F7F7),
+      backgroundColor: Colors.grey[200],
+
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         toolbarHeight: 70,
-        title: Text('Profile', style: TextStyle(fontSize: 15)),
-        backgroundColor: const Color(0xffF7F7F7),
+        title: const Text(
+          'Profile',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontFamily: 'Outfit',
+              fontWeight: FontWeight.w500),
+        ),
+        // backgroundColor: Color(0xffF7F7F7),
+        backgroundColor: Colors.grey[200],
+        elevation: 0,
         actions: [
           IconButton(
               onPressed: () {},
@@ -35,16 +48,225 @@ class UserPage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          CircleAvatar(
-            radius: 80,
-            child: Icon(
-              Icons.person,
-              size: 90,
-              color: Colors.black,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(22)),
+          child: Column(children: [
+            const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1630568667432-0fa3609f0a32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"),
+              radius: 80,
+              // child: Icon(
+              // // Icons.person,
+              // size: 90,
+              // color: Colors.black,
+              // ),
             ),
-          )
-        ]),
+            RichText(text: const TextSpan(text: 'Tushar')),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 15, color: Colors.transparent),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white),
+              height: getProportionateScreenHeight(170),
+              width: getProportionateScreenWidth(330),
+              child: const Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Email",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "1930331246040@dbatu.ac.in",
+                              // style: TextStyle(fontSize: 18),
+                              textAlign: TextAlign.end,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(color: Colors.black),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Date of Birth:",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Expanded(
+                              child: Text(
+                            "24.03.2002",
+                            textAlign: TextAlign.end,
+                          ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(color: Colors.black),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "PRN:",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Expanded(
+                              child: Text(
+                            "1930331246040",
+                            textAlign: TextAlign.end,
+                          ))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 15, color: Colors.transparent),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white),
+              height: getProportionateScreenHeight(170),
+              width: getProportionateScreenWidth(330),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.lock, color: Colors.red),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Change Password",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          // SizedBox(
+                          //   width: 70,
+                          // ),
+                          // Icon(Icons.arrow_forward_ios)
+                          Expanded(
+                            child: IconButton(
+                                alignment: Alignment.topRight,
+                                onPressed: () {},
+                                icon: Icon(Icons.arrow_forward_ios)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(color: Colors.black),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.help, color: Colors.green),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Help",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Expanded(
+                              child: IconButton(
+                            alignment: Alignment.centerRight,
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_forward_ios),
+                          ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Divider(color: Colors.black),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.app_settings_alt_outlined,
+                            color: Colors.pink,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "About App",
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Expanded(
+                              child: IconButton(
+                            alignment: Alignment.centerRight,
+                            icon: Icon(Icons.arrow_forward_ios),
+                            onPressed: () {},
+                          ))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
       ),
     );
     // Padding(
